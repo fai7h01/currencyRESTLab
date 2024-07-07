6d6734c83e6fdd0057482890dada5c099dff7336
fai7h01 <l.varsimashvil10@gmail.com>
2024-07-07 22:24:46 +0400

    initial commit

--- src/main/java/com/cydeo/CurrencyRestLabApplication.java
+++ src/main/java/com/cydeo/CurrencyRestLabApplication.java
@@ -1,13 +1,20 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CurrencyRestLabApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CurrencyRestLabApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
     }
 
 }
